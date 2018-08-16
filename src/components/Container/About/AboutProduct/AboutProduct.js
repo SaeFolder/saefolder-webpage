f4dbceb7b7f3a881af798bfe1c7875de49640938
KZPraY <rhanfwlq980@naver.com>
2018-08-16 17:55:50 +0900

    main page

--- src/components/Container/About/AboutProduct/AboutProduct.js
+++ src/components/Container/About/AboutProduct/AboutProduct.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import styles from './AboutProduct.scss';
 import semicircle_right from '../../../../assets/img/semicircle_right.png';
+import parallelogram from '../../../../assets/img/parallelogram.png';
+import arrow_1 from '../../../../assets/img/arrow_1.png';
+import arrow_2 from '../../../../assets/img/arrow_2.png';
+import img_products_1 from '../../../../assets/img/img_products_1.jpg';
+import img_products_2 from '../../../../assets/img/img_products_2.jpg';
 
 class AboutProduct extends Component {
   state = {  }
@@ -23,7 +28,23 @@ class AboutProduct extends Component {
 
         {/* Contents */}
         <div className={styles.contents}>
-        contents
+
+          <div className={styles.space_left}>
+            <img src={parallelogram} className={styles.parallelogram_1} />
+            <img src={arrow_2} className={styles.arrow_2} />
+            <img src={arrow_1} className={styles.arrow_1} />
+          </div>
+
+          <div className={styles.products_1}>
+            <img src={img_products_1} className={styles.img_products_1} />
+          </div>
+
+          <div className={styles.products_2}>
+          </div>
+
+          <div className={styles.space_right}>
+          </div>
+
         </div>
       </div>
     );
